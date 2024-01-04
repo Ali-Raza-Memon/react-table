7d3084ac489c539883108d62dd8defa6885aa3fb
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2024-01-04 17:43:44 +0500

    columns are grouped

--- src/components/columns.jsx
+++ src/components/columns.jsx
@@ -5,31 +5,61 @@ export const COLUMNS = [
         Footer: "Id",
         accessor: "id",
     },
-    {
-        Header: "First Name",
-        Footer:"First Name",
-        accessor: "first_name",
-    },
-    {
-        Header: "Last Name",
-        Footer: "Last Name",
-        accessor: "last_name",
-    },
-    {
-        Header: "Date of Birth",
-        Footer: "Date of Birth",
-        accessor: "date_of_birth",
-    },
+
+
+
+]
+
+
+export const GROUPED_COLUMNS = [
 
     {
-        Header: "Country",
-        Footer: "Country",
-        accessor: "country",
+        Header : "Id",
+        Footer:'Id',
+        accessor: "id"
     },
     {
-        Header: "Phone",
-        Footer:"Phone",
-        accessor: "phone",
+        Header :"Name",
+        Footer: "Name",
+        columns :[
+            
+            {
+                Header: "First Name",
+                Footer:"First Name",
+                accessor: "first_name",
+            },
+            {
+                Header: "Last Name",
+                Footer: "Last Name",
+                accessor: "last_name",
+            },
+
+        ]
+    },
+    {
+        Header : "Info",
+        Footer : "Info",
+
+        columns: [
+            {
+                Header: "Date of Birth",
+                Footer: "Date of Birth",
+                accessor: "date_of_birth",
+            },
+        
+            {
+                Header: "Country",
+                Footer: "Country",
+                accessor: "country",
+            },
+            {
+                Header: "Phone",
+                Footer:"Phone",
+                accessor: "phone",
+            }
+        ]
+
+
     }
 
 ]
